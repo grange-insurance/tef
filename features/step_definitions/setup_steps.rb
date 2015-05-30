0fbedb7007a43f4864d0ea7db186cf3c5f8b8b7c
Eric Kessler <morrow748@gmail.com>
2015-05-30 17:49:15 -0400

    Reorganizing common test code.
    Moving test code that is used by both the tef and tef-cucumber
    projects to locations that are more easily shareable.

--- features/step_definitions/setup_steps.rb
+++
@@ -1,8 +0,0 @@
-And(/^a manager node is running$/) do
-  @manager_pid = Process.spawn('start "Manager" cmd /c bundle exec start_tef_manager')
-  Process.detach(@manager_pid)
-end
-
-And(/^no TEF nodes are running$/) do
-  kill_existing_tef_processes
-end

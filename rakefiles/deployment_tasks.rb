f4fee90d15e81a1f0bfd4fc21460aa7fa45de856
Ashton Freidenberger <no@no.com>
2015-06-01 14:16:12 -0400

    removing 15 second sleep from rake file

--- rakefiles/deployment_tasks.rb
+++ rakefiles/deployment_tasks.rb
@@ -6,7 +6,6 @@ namespace 'tef' do
   desc 'Spin up local versions of the TEF'
   task :create_tef_farm do
     Rake::Task['tef:create_manager'].invoke
-    sleep 15
     Rake::Task['tef:create_queuebert'].invoke
     Rake::Task['tef:create_worker'].invoke
   end

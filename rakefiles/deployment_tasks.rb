b6c0b94edf50a1e047c5ac2619de165033a16115
Eric Kessler <morrow748@gmail.com>
2015-06-04 09:06:51 -0400

    Cleaning out more Cucumber specific stuff.
    Fixed a deployment rake task that was erroneously trying to spin up
    a Cucumber component (it was Queuebert).

--- rakefiles/deployment_tasks.rb
+++ rakefiles/deployment_tasks.rb
@@ -7,7 +7,6 @@ namespace 'tef' do
   desc 'Spin up local versions of the TEF'
   task :create_tef_farm do
     Rake::Task['tef:create_manager'].invoke
-    Rake::Task['tef:create_queuebert'].invoke
     Rake::Task['tef:create_worker'].invoke
   end
 

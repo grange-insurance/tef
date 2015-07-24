51bd80e9f8e4524ed836871ee08338bb744e1629
Eric Kessler <morrow748@gmail.com>
2015-07-24 17:05:19 -0400

    Database connection update.
    Changed the database connection method in the development gem is
    called so that it can be given several different ways of accessing
    database configuration data.

--- gems/tef-development/lib/tef/development/testing/database.rb
+++ gems/tef-development/lib/tef/development/testing/database.rb
@@ -5,17 +5,22 @@ module TEF
   module Development
     module Testing
 
-      def self.connect_to_test_db(db_config_file = nil)
+      def self.connect_to_test_db(options = {})
         ActiveRecord::Base.time_zone_aware_attributes = true
         ActiveRecord::Base.default_timezone = :local
 
-        unless db_config_file
-          raise 'Environmental variable TEF_DB_CONFIG_PATH must be set or a configuration file provided' if ENV['TEF_DB_CONFIG_PATH'].nil?
+        db_config = options[:db_config]
+        db_config_file = options[:db_config_file]
 
-          db_config_file = ENV['TEF_DB_CONFIG_PATH']
+        unless db_config
+          unless db_config_file
+            raise 'Environmental variable TEF_DB_CONFIG_PATH must be set or a configuration file provided' if ENV['TEF_DB_CONFIG_PATH'].nil?
+
+            db_config_file = ENV['TEF_DB_CONFIG_PATH']
+          end
+
+          db_config = YAML.load_file(db_config_file)
         end
-
-        db_config = YAML.load_file(db_config_file)
 
         ActiveRecord::Base.establish_connection(db_config)
         ActiveRecord::Base.logger = Logger.new(File.open('tef_test_database.log', 'a'))

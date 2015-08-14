ff04fac4d9df3cefc8f3daf7bd124ff4ddd90685
Eric Kessler <morrow748@gmail.com>
2015-08-14 15:18:08 -0400

    'Turning off' a TEF component.
    Removed the amqlog gem from the list of framework components used by
    the various build and testing tasks because it doesn't seem to
    build/install correctly and no other component seems to use it and
    none of the active developers know what it is for in the first place.

--- rakefiles/common_tasks.rb
+++ rakefiles/common_tasks.rb
@@ -4,7 +4,8 @@ module TEF
   def self.component_locations
     {
         'task_runner' => 'gems/task_runner',
-        'amqlog' => 'gems/amqlog',
+        # todo - add this back in when we know what it is
+        # 'amqlog' => 'gems/amqlog',
         'tef-core' => 'gems/tef-core',
         'tef-keeper' => 'gems/tef-keeper',
         'tef-manager' => 'gems/tef-manager',

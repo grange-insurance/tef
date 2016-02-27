92b188b3c4a8e1d041847b9b410af904ee5c92e9
Eric Kessler <morrow748@gmail.com>
2016-02-27 10:19:58 -0500

    Adding clarification comments to code.
    Called out some code that relies on the source code of the various
    sub-projects to be in a certain relationship to each other.

--- gems/tef-development/lib/tef/development/deployment_tasks.rb
+++ gems/tef-development/lib/tef/development/deployment_tasks.rb
@@ -5,6 +5,7 @@ namespace 'tef' do
 
   desc 'Spin up a manager from the current source code'
   task :create_manager => [:set_tef_environment] do
+    # Assuming that the entire TEF project is present
     here = File.dirname(__FILE__)
     path_to_manager_binary = "#{here}/../../../../tef-manager/bin/start_tef_manager"
 
@@ -14,6 +15,7 @@ namespace 'tef' do
 
   desc 'Spin up a worker from the current source code'
   task :create_worker => [:set_tef_environment] do
+    # Assuming that the entire TEF project is present
     here = File.dirname(__FILE__)
     path_to_worker_binary = "#{here}/../../../../tef-worker/bin/start_tef_worker"
 
